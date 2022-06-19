5643130666cec5a9fac55326341f5b60d0e9930c
bandrewk <6756767+bandrewk@users.noreply.github.com>
2022-06-19 16:06:33 +0200

    Added input

---
+++ src/components/Layout/Input.js
@@ -0,0 +1,84 @@
+import classes from "./Input.module.css";
+import { useState } from "react";
+
+const Input = (props) => {
+  const [inputText, setInputText] = useState("");
+  const [hasError, setHasError] = useState(false);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // Last validity check
+    if (inputText.trim().length < 8) {
+      setHasError(true);
+      return;
+    }
+
+    const artikelNr = inputText.trim().split(".").join("");
+
+    props.addItemHandler(artikelNr);
+
+    setInputText("");
+  };
+
+  const onInputChangeHandler = (event) => {
+    // Remove whitespaces
+    const value = event.target.value.trim();
+
+    // Check for letters
+    if (/[a-zA-Z]/g.test(value)) {
+      setHasError(true);
+    } else setHasError(false);
+
+    setInputText(value);
+  };
+
+  const errorDesc = hasError ? (
+    <p className={`${classes.inputDesc} ${classes.error}`}>
+      Fehlerhafte Eingabe! Bitte an das Format halten. Es sind keine Buchstaben
+      erlaubt!
+    </p>
+  ) : (
+    ""
+  );
+
+  return (
+    <section className={classes["section-input"]}>
+      <div className="container">
+        <form onSubmit={onSubmitHandler}>
+          <label for="articleNumber" className="heading-tertiary">
+            Artikelnummer
+          </label>
+
+          <input
+            id="articleNumber"
+            required
+            className={`${classes["input-articleNumber"]} ${
+              hasError ? `${classes.error}` : ``
+            }`}
+            type="text"
+            placeholder="404.567.57"
+            value={inputText}
+            onChange={onInputChangeHandler}
+            a
+          />
+          <button
+            disabled={hasError ? true : false}
+            className={classes["btn-primary"]}
+          >
+            Hinzufügen
+          </button>
+        </form>
+        {errorDesc}
+        {!hasError && (
+          <p className={classes.inputDesc}>
+            Artikelnummer mit oder ohne Punkte eingeben und mit ENTER oder
+            `Hinzufuegen` bestätigen.
+          </p>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Input;
--- src/components/Layout/Statistics.js
+++ src/components/Layout/Statistics.js
@@ -4,33 +4,33 @@ const Statistics = (props) => {
   if (props.stats.totalItems === 0) return;
 
   return (
-    <section class={classes["section-statistics"]}>
-      <div class="container">
-        <p class="heading-tertiary">Statistik</p>
+    <section className={classes["section-statistics"]}>
+      <div className="container">
+        <p className="heading-tertiary">Statistik</p>
         <div>
-          <ul class={classes["stats-list"]}>
-            <li class={classes["stats-list--item"]}>
+          <ul className={classes["stats-list"]}>
+            <li className={classes["stats-list--item"]}>
               <ion-icon name="chevron-forward-outline"></ion-icon>
               <span>
                 Gesamtersparnis: <span>€ {props.stats.totalDiscount}</span> (
                 {props.stats.totalDiscountInPercentage}%)
               </span>
             </li>
-            <li class={classes["stats-list--item"]}>
+            <li className={classes["stats-list--item"]}>
               <ion-icon name="chevron-forward-outline"></ion-icon>
               <span>
                 Gesamtpreis (&#127465;&#127466;):{" "}
                 <span>€ {props.stats.totalPriceDE}</span>
               </span>
             </li>
-            <li class={classes["stats-list--item"]}>
+            <li className={classes["stats-list--item"]}>
               <ion-icon name="chevron-forward-outline"></ion-icon>
               <span>
                 Gesamtpreis (&#127477;&#127473;):{" "}
                 <span>€ {props.stats.totalPricePLEur}</span>
               </span>
             </li>
-            <li class={classes["stats-list--item"]}>
+            <li className={classes["stats-list--item"]}>
               <ion-icon name="chevron-forward-outline"></ion-icon>
               <span>Anzahl an Produkten: {props.stats.totalItems} Stück</span>
             </li>
